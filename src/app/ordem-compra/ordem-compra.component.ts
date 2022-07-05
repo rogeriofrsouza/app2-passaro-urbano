958307a970478291aa5e34591e43ae5ce034b9ef
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2022-07-05 16:20:34 -0300

    Formulário Ordem Compra - Template Forms

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { OrdemCompraService } from './../shared/services/ordem-compra.service';
 
@@ -9,9 +10,16 @@ import { OrdemCompraService } from './../shared/services/ordem-compra.service';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  @ViewChild('formulario') public formulario!: NgForm;
+
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
     
   }
+
+  public confirmarCompra(): void {
+    console.log(this.formulario);
+  }
+
 }
