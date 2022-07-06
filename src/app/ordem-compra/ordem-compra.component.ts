d9127adaf227f0d3ce814480814db5e26a2a481c
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2022-07-06 17:07:26 -0300

    Implementação Reactive Forms, requisição post e ajuste pedido.model

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,6 +1,7 @@
-import { Pedido } from './../shared/models/pedido.model';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { Pedido } from './../shared/models/pedido.model';
 import { OrdemCompraService } from './../shared/services/ordem-compra.service';
 
 @Component({
@@ -10,6 +11,15 @@ import { OrdemCompraService } from './../shared/services/ordem-compra.service';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public formulario: FormGroup = new FormGroup({
+    'endereco': new FormControl(null, [ Validators.required, Validators.minLength(3), Validators.maxLength(40) ]),
+    'numero': new FormControl(null, [ Validators.required, Validators.minLength(1), Validators.maxLength(8) ]),
+    'complemento': new FormControl(null),
+    'formaPagamento': new FormControl(null, [ Validators.required ])
+  });
+
+  public idPedidoCompra?: number;
+
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
@@ -17,6 +27,23 @@ export class OrdemCompraComponent implements OnInit {
   }
 
   public confirmarCompra(): void {
+    console.log(this.formulario);
+
+    if (this.formulario.status === 'INVALID') {
+      this.formulario.get('endereco')?.markAsTouched();
+      this.formulario.get('numero')?.markAsTouched();
+      this.formulario.get('complemento')?.markAsTouched();
+      this.formulario.get('formaPagamento')?.markAsTouched();
+      
+    } else {
+      let pedido: Pedido = this.formulario.value;
+      this.ordemCompraService.efetivarCompra(pedido).subscribe({
+        next: (pedido: Pedido) => {
+          console.log(pedido);
+          this.idPedidoCompra = pedido.id;
+        }
+      });
+    }
   }
 
 }
